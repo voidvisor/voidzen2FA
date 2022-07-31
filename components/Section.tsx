035247d2111b2db438ea0beaa0b424a59b04446c
voidvisor <43892995+voidvisor@users.noreply.github.com>
2022-08-01 01:48:26 +0300

    Move section component to separate file

---
+++ components/Section.tsx
@@ -0,0 +1,36 @@
+import React, {type PropsWithChildren} from 'react';
+import { useColorScheme, View, Text } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { styles } from '../App';
+
+const Section: React.FC<
+  PropsWithChildren<{
+    title: string;
+  }>
+> = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+export default Section;
