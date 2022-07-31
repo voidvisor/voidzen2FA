66cbf770e77e10214b1db98dcb85edeb69a04394
voidvisor <43892995+voidvisor@users.noreply.github.com>
2022-08-01 01:50:30 +0300

    Move Auth.tsx to views folder

--- components/views/Auth.tsx
+++ components/views/Auth.tsx
@@ -3,7 +3,6 @@ import {
     SafeAreaView,
     ScrollView,
     StatusBar,
-    StyleSheet,
     Text,
     useColorScheme,
     View,
@@ -15,36 +14,8 @@ import {
     LearnMoreLinks,
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import { styles } from '../../App';
+import Section from '../Section';
 
 const Auth = () => {
     const isDarkMode = useColorScheme() === 'dark';
@@ -55,7 +26,6 @@ const Auth = () => {
 
     return(
         <SafeAreaView style={backgroundStyle}>
-          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
           <ScrollView
             contentInsetAdjustmentBehavior="automatic"
             style={backgroundStyle}>
@@ -86,24 +56,5 @@ const Auth = () => {
         </SafeAreaView>
     );
 }
-
-const styles = StyleSheet.create({
-    sectionContainer: {
-      marginTop: 32,
-      paddingHorizontal: 24,
-    },
-    sectionTitle: {
-      fontSize: 24,
-      fontWeight: '600',
-    },
-    sectionDescription: {
-      marginTop: 8,
-      fontSize: 18,
-      fontWeight: '400',
-    },
-    highlight: {
-      fontWeight: '700',
-    },
-});
 
 export default Auth;
