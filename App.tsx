eed21231e5d9fd44156ffbe2cd585ef49f39f576
voidvisor <43892995+voidvisor@users.noreply.github.com>
2022-08-01 01:51:22 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
-import Auth from './components/Auth';
-
+import React, { Component } from 'react';
+import Auth from './components/views/Auth';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Button } from 'react-native';
