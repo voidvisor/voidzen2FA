c1c6edef38e30cee927dba9a2c55acd745b4b39f
voidvisor <43892995+voidvisor@users.noreply.github.com>
2022-10-19 21:17:57 +0300

    Updates for adding stack navigator and accounts

--- components/views/Auth.tsx
+++ components/views/Auth.tsx
@@ -8,7 +8,7 @@ import {
   View,
 } from 'react-native';
 import { authStyle as style } from '../core/Style';
-import { Appbar, Button, Headline, Subheading, useTheme } from 'react-native-paper';
+import { Button, Divider, FAB, Headline, Subheading, useTheme } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import ReactNativeBiometrics from 'react-native-biometrics';
 import EncryptedStorage from 'react-native-encrypted-storage';
@@ -16,9 +16,11 @@ import Account from '../Account';
 
 const rnBiometrics = new ReactNativeBiometrics();
 
-const Auth = () => {
-  const [auth, setAuth] = React.useState(false);
-  const [accList, setAccList] = React.useState([{uuid: 'test'}]);
+const Auth = ({auth, setAuth, setBack, navigation}) => {
+  const [accList, setAccList] = React.useState([]);
+  const [fabOpen, setFabOpen] = React.useState({open: false});
+
+  const fabStateChange = ({open}) => setFabOpen({open});
 
   const { colors } = useTheme();
 
@@ -38,7 +40,7 @@ const Auth = () => {
       try {
         const list = await EncryptedStorage.getItem('list');
         if (list) {
-          setAccList(JSON.parse(list!));
+          setAccList(JSON.parse(list));
         }
       } catch (error) {
         console.error(error);
@@ -49,7 +51,14 @@ const Auth = () => {
   React.useEffect(() => {
     promptBiometrics();
     retrieveList();
-  }, [auth])
+
+    const focus = navigation.addListener('focus', () => {
+      setBack(false);
+      retrieveList();
+    })
+
+    return focus;
+  }, [auth, navigation])
 
   const renderAuth = ({ item }) => (
     <Account uuid={item.uuid} />
@@ -58,21 +67,29 @@ const Auth = () => {
   if (auth) {
   return (
     <>
-    <Appbar.Header>
-      <Appbar.Action icon="lock" color={colors.surface} onPress={() => setAuth(false)} style={style.lockButton} />
-    </Appbar.Header>
-    <FlatList contentContainerStyle={style.flatListContainer} renderItem={renderAuth} data={accList} ListEmptyComponent={
+    <FlatList contentContainerStyle={style.flatListContainer} renderItem={renderAuth} data={accList} ItemSeparatorComponent={() => {return (<Divider />)}} ListEmptyComponent={
       <View style={style.lockedView}>
         <Icon name='help' size={200} color={colors.accent} />
         <Headline>It's empty here.</Headline>
         <Subheading>Add some accounts using the plus button!</Subheading>
       </View>
     } />
+    <FAB.Group
+      visible
+      open={fabOpen.open}
+      icon={fabOpen.open ? 'close' : 'plus'}
+      backdropColor='rgba(0,0,0,0.5)'
+      actions={[
+        {icon: 'camera', label: 'Scan QR Code', onPress: () => {}},
+        {icon: 'plus', label: 'Enter Secret Manually', onPress: () => {navigation.navigate('Manual');setBack(true)}},
+      ]}
+      onStateChange={fabStateChange}
+    />
     </>
   )
   } else {
     return (
-    <View style={style.lockedView}>
+      <View style={style.lockedView}>
         <Icon name='lock' size={200} color={colors.accent} />
         <Headline style={{paddingBottom: 6}}>Authenticator Locked</Headline>
         <Button mode='contained' dark={true} onPress={() => promptBiometrics()}>PRESS TO UNLOCK</Button>
